078d1202a8ac744c100d12d3ce9ef95c8997bc70
Fernando <fernando_ruans@hotmail.com>
2023-01-19 16:01:58 -0300

    Ajustando Com JavaScript!

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-const mensagem = "Hello"
-alert(mensagem + " World!")
+const form = document.querySelector('#form-habits')
+const nlwSetup = new NLWSetup(form)
+
+const data = {
+  run: ['01-01', '01-02', '01-03', '01-06','01-07', '01-08'],
+  takePills: ['01-02'],
+  journal: ['01-02'],
+
+}
+nlwSetup.setData(data)
+nlwSetup.load()
